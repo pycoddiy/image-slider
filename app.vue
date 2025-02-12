b21d5f3ca7af57aedf138c08bf0b6cdf78c69a77
Sergey Maydanov <pycoddiy@gmail.com>
2025-02-12 12:38:59 -0600

    Initial commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="m-10">
+    <Carousel :slides="slides" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    slides: [
+      "https://picsum.photos/id/1032/900/400",
+      "https://picsum.photos/id/1033/900/400",
+      "https://picsum.photos/id/1037/900/400",
+      "https://picsum.photos/id/1035/900/400",
+      "https://picsum.photos/id/1036/900/400",
+    ],
+  }),
+}
+</script>
+
+<style scoped>
+</style>
---
+++ components/Carousel.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="carousel">
+      <div class="carousel-inner">
+        <CarouselItem v-for="(slide, index) in slides" :slide="slide" :key="`item-${index}`" :current-slide="currentSlide" :index="index" />
+        <P>{{ currentSlide }}</P>
+      </div>
+    </div>
+</template>
+
+<script setup>
+  import { useInterval } from '@vueuse/core'
+  const { slides } = defineProps(['slides']);
+
+  let currentSlide = 1;
+
+  const nextSlide = (count) => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    console.log(currentSlide);
+  };
+
+  useInterval(5000, {callback: nextSlide})  
+</script>
+
+<style scoped>
+  .carousel {
+    display: flex;
+    justify-content: center;
+  }
+
+  .carousel-inner {
+    position: relative;
+    width: 900px;
+    height: 400px;
+    overflow: hidden;
+  }
+</style>
